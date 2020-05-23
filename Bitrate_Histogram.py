5204cb08f0deb1fdf4ad61f103738e4c1f869e58
antatura <47680985+antatura@users.noreply.github.com>
2020-05-23 14:57:00 +0800

    Add files via upload

--- Bitrate_Histogram.py
+++ Bitrate_Histogram.py
@@ -1,5 +1,5 @@
 # Requirements: ffprobe.exe, matplotlib, numpy
-
+# Only v:0 of a media file
 
 import subprocess
 import argparse
@@ -30,46 +30,57 @@ csv_list = list(csv.stdout)
 duration_list = list(duration_b.stdout)
 
 
-sec = 1
+
 s_size = 0
 list_size = []
 
+
 for a in range(len(csv_list)):
-    if csv_list[a] != b'\r\n':
-        aa = str(csv_list[a],'ascii')    # '0.016667,117\r\n'
+    if csv_list[a] != b'\r\n':   
+        aa = str(csv_list[a],'ascii')      # '0.016667,117\r\n'
         a_ = aa.index(',')
-        ax = float(aa[:a_])    
-        ay = int(aa[a_+1:-2])    # ax=0.016667, ay=117
+        ax = float(aa[:a_])
+        ay = int(aa[a_+1:-2])          # ax=0.016667, ay=117
+        if a == 0:
+            ax0 = int(ax)     # 11.345345 ==> 11
+            ax0_f = ax
+            sec = ax0 + 1
         if ax < sec:
-            s_size += ay*8/1000    # kbit
+            s_size += ay*8/1000       # kbit
         else:
             list_size.append(s_size)
             s_size = ay*8/1000
             sec += 1        
 list_size.append(s_size)
+ls_0 = [0 for x in range(ax0)]
+list_size = ls_0 + list_size
 
 
 duration = float(str(duration_list[0],'ascii'))
+end = ax0_f + duration
 maxi = int(np.max(list_size))
 mean = int(np.sum(list_size)/duration)
 
 endtime = datetime.now()
-dur = str(endtime-startime)
-w_title = '['+dur+']'+' '*3+args.input
+eal = str(endtime-startime)
+w_title = '['+eal+']'+' '*3+args.input
+xlabel = 'Start: '+str(ax0_f)+'    Duration: '+str(duration)+'    End: '+str(end)
+##print(list_size[:14])
+
 
 
 plt.figure().canvas.set_window_title(w_title)
 plt.title("Bitrate Histogram",fontsize=16,fontweight='bold')
-plt.xlabel("time",fontsize=12,fontweight='bold')
+plt.xlabel(xlabel,fontsize=12,fontweight='bold')
 plt.ylabel("kbit per sec",fontsize=12,fontweight='bold')
-plt.grid(True)
+plt.grid(False)
 plt.bar(range(len(list_size)),list_size,width=1,ec='#195f90',align='edge')
 plt.axhline(y=maxi,color='r',lw=2,ls='--',label='max: '+str(maxi))
 plt.axhline(y=mean,color='g',lw=2,ls='--',label='mean: '+str(mean))
-plt.axvline(x=ax,color='b',lw=3,ls=':')
+plt.axvline(x=ax0_f,color='y',lw=2.5,ls=':')
+plt.axvline(x=end,color='y',lw=2.5,ls=':')
 plt.legend(prop={'size': 18})
 plt.show()
 
         
-
 
